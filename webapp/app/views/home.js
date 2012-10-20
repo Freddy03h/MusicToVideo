691c5e013f1ca202c7fb9af20272452d53aaec1c
freddyharris <freddy03h@gmail.com>
2012-10-20 06:23:07 +0200

    use Javascript SDK

--- webapp/app/views/home.js
+++ webapp/app/views/home.js
@@ -11,6 +11,39 @@ function(app, template) {
     id:"home",
     template: template,
     events: {
+      "click #connect": 'connect'
+    },
+    connect : function(e){
+      e.preventDefault();
+      console.log(e);
+
+      DM.login(function(response)
+      {
+          if (response.session)
+          {
+              // user successfully logged in
+              console.log(response.session);
+
+              DM.api('/me/playlists', 'get', function(response)
+              {
+                  if (!response || response.error)
+                  {
+                      alert('Error occured');
+                  }
+                  else
+                  {
+                      alert('Liked successfuly');
+                      console.log(response);
+                  }
+              });
+
+          }
+          else
+          {
+              console.log('et non');
+              // user cancelled login
+          }
+      });
     }
   });
 
